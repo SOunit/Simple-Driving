dd7ed72711babcba19bf5a48817c4a6233eb32d4
SOUnit <workbookwork7@gmail.com>
2022-11-27 11:36:29 -0800

    add steer car logic

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     [SerializeField]
-    float speed = 10f;
+    private float speed = 10f;
 
     [SerializeField]
-    float speedGainPerSecond = 0.2f;
+    private float speedGainPerSecond = 0.2f;
+
+    [SerializeField]
+    private float turnSpeed = 200f;
+
+    private int steerValue;
 
     void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
